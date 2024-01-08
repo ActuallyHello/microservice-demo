ccbd8616eb8af5385529495fd2bfb8075747e42a
HappyMq <happy.xmas1@mail.ru>
2024-01-08 10:47:54 +0300

    inter service communication

--- inventory-service/src/main/java/com/happyfxmas/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/happyfxmas/inventoryservice/controller/InventoryController.java
@@ -1,14 +1,17 @@
 package com.happyfxmas.inventoryservice.controller;
 
-import com.happyfxmas.inventoryservice.domain.Inventory;
+import com.happyfxmas.inventoryservice.dto.InventoryResponse;
 import com.happyfxmas.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,4 +25,11 @@ public class InventoryController {
     public boolean isInStock(@PathVariable("sku-code") String skuCode) {
         return inventoryService.isInStock(skuCode);
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isListInStock(@RequestParam List<String> skuCode) {
+        return inventoryService.isListInStock(skuCode);
+    }
+
 }
